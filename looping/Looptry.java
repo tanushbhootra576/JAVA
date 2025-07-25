f251c5ac03ed5240f6074628270726053d349512
tanushbhootra576 <193266446+tanushbhootra576@users.noreply.github.com>
2025-07-25 15:09:43 +0530

    looping!!

---
+++ looping/Looptry.java
@@ -0,0 +1,10 @@
+public class Looptry {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println("Current value of i: " + i);
+            i++;
+        }
+        System.out.println("Loop completed.");
+    }
+}
