25cd66ae10fa21ba1209128ae0bf5e405cadb01a
tanushbhootra576 <193266446+tanushbhootra576@users.noreply.github.com>
2025-07-30 15:04:55 +0530

    updated

---
+++ typeConversion/Main.java
@@ -0,0 +1,17 @@
+// You are using Java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        int pressure = s.nextInt();  
+        float temp = s.nextFloat();  s.nextLine();
+        String status = s.nextLine();  
+
+
+        System.out.println("Pressure = " + pressure);
+        System.out.println("Temperature = " + temp.2f); 
+        System.out.println("Sensor: " + status);
+    }
+}a
